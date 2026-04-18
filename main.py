24cd1673242d56a48a121495bd1e2d085cdd3776
josephferguson0814 <jferguson0814@gmail.com>
2026-04-18 12:34:16 -0400

    Output now displays token usage

--- main.py
+++ main.py
@@ -16,6 +16,16 @@ def main():
     client = genai.Client(api_key=api_key)
     content = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
 
+    #Gets token usage
+    if content.usage_metadata == None:
+        raise RuntimeError("unable to connect to Gemini")
+    prompt_token_usage = content.usage_metadata.prompt_token_count
+    candidate_token_usage = content.usage_metadata.candidates_token_count
+
+    print("User prompt: Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    print(f"Prompt tokens: {prompt_token_usage}")
+    print(f"Response tokens: {candidate_token_usage}")
+    print("Response:")
     print(content.text)
 
     
