f750da2c2d4a08b5105ee59a3b3be2291edb5e71
josephferguson0814 <jferguson0814@gmail.com>
2026-04-18 12:47:12 -0400

    prompt is now one of the args to run program

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 
 def main():
+    #Uses argparse to take an argument from the command line for the prompt
+    parser = argparse.ArgumentParser(description="AI Agent")
+    parser.add_argument("user_prompt", type=str, help="User Prompt")
+    args = parser.parse_args()
+
     #Loads API key
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -14,7 +20,7 @@ def main():
     
     #Connects with gemini and prompts it
     client = genai.Client(api_key=api_key)
-    content = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    content = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
 
     #Gets token usage
     if content.usage_metadata == None:
@@ -22,7 +28,7 @@ def main():
     prompt_token_usage = content.usage_metadata.prompt_token_count
     candidate_token_usage = content.usage_metadata.candidates_token_count
 
-    print("User prompt: Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {prompt_token_usage}")
     print(f"Response tokens: {candidate_token_usage}")
     print("Response:")
